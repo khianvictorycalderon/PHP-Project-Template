a71f6f85225682fbd961d460b237fd6d51ba5d33
Khian Calderon <ckhianvictory@gmail.com>
2024-12-03 22:26:41 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a Template Project
+
+Use this if you want to make a server side web project using PHP.
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./assets/index.css" />
+    <script src="assets/index.js"></script>
+    <title>PHP Template</title>
+  </head>
+  <body>
+    <?php
+		echo "Hello World";
+	?>
+  </body>
+</html>
